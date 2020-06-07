315b51c2f74c6e2a5a3c94cc2479922022ed72e4
Tushar Khan <tushar.54k@gmail.com>
2020-06-07 03:06:09 -0400

    added animation

--- simulate.js
+++ simulate.js
@@ -1,5 +1,4 @@
 import * as Dynamics from './dynamics.js';
-import * as Geometry from './geometry.js';
 import Vector from './vector.js';
 
 class Layer {
@@ -13,13 +12,27 @@ class Layer {
 }
 
 export class World {
-    constructor(ctx, debug=false) {
+    /**
+     * @param {*} ctx the canvas drawing context.
+     * @param {boolean} debug whether to run in debug mode.
+     * @param {number} fps maximum frames per second.
+     * @param {number} cps maximum calculations per second (i.e. simulation granularity).
+     */
+    constructor(ctx, debug=false, fps=60, cps=200) {
+        // rendering properties
         this.ctx = ctx;
         this.debug = debug;
 
+        // body managers
         this.staticBodies = [];
         this.kinematicBodies = [];
         this.dynamicBodies = [];
+
+        // time stepping properies
+        this.fps = fps;
+        this.dt = 1 / cps;
+        this.acc = 0;
+        this.stepLimit = 10; // maximum nuumber of steps per update
     }
 
     get allBodies() {
@@ -70,7 +83,7 @@ export class World {
             const collision = new Dynamics.Collision(a, b);
             if (collision.detect()) {
                 collision.resolve();
-                console.log('COLLISION!')
+                //console.log('Collision detected')
             }
         });
     }
@@ -83,24 +96,73 @@ export class World {
     }
 
     /**
-     * Steps the simulation time forward.
-     * @param {Number} t still deciding what this means
+     * Resolves collisions and updates the simulated bodies by `dt`.
+     * @param {Number} dt change in time.
      */
-    step(t) {
+    step(dt) {
+        // integrate forces to produce new velocities
+        this.dynamicBodies.concat(this.kinematicBodies).forEach(b => {
+            b.velocity = b.velocity.add(b.acceleration.scale(dt));
+            b.force = Vector.ZERO;
+        })
+
+        // generate collision impulses to correct velocity errors
         this.resolveCollisions();
-        this.kinematicBodies.forEach(e => e.update());
-        this.dynamicBodies.forEach(e => e.update());
+        this.dynamicBodies.concat(this.kinematicBodies).forEach(b => {
+            b.velocity = b.velocity.add(b.impulse.scale(b.inv_mass));
+            b.impulse = Vector.ZERO;
+        })
+
+        // update positions using new velocities
+        this.dynamicBodies.concat(this.kinematicBodies).forEach(b => {
+            b.translate(b.velocity.scale(dt));
+            b.translate(b.correction)
+            b.correction = Vector.ZERO;
+        })
+
+/*
+        this.resolveCollisions();
+        this.kinematicBodies.forEach(e => e.update(dt));
+        this.dynamicBodies.forEach(e => e.update(dt));
+
+*/
 
         // gravity
-      //  this.dynamicBodies.forEach(e => e.addForce(new Vector(0, 0.15).scale(e.mass)));
+        //this.dynamicBodies.forEach(e => e.addForce(new Vector(0, 100).scale(e.mass)));
+    }
+
+    /**
+     * Performs `this.cps` simulation steps per second to update the simulation.
+     * @param {*} t time of the simulation in seconds.
+     */
+    update(t) {
+        // update the accumulator
+        if (this.lastUpdate != undefined) {
+            this.acc += t - this.lastUpdate;
+        }
+        this.lastUpdate = t;
+
+        // clamp upper value of accumulator to reduce number of steps when too much load
+        const accMax = this.dt * this.stepLimit;
+       // console.log('acc:', this.acc, 'max:', accMax, 'min:', this.dt)
+        if (this.acc > accMax) {
+            console.log('Simulation throttled:', this.acc - accMax, 'second delay.')
+            this.acc = accMax;
+        }
+
+        // step the simulation in discrete `dt` sized chunks of time
+        for (; this.acc >= this.dt; this.acc -= this.dt) {
+            this.step(this.dt);
+        }
     }
 
     /**
      * Renders every body onto the cavnas using their `.draw()` method.
-     * @param {Number} t still deciding what this means
      */
     render(t) {
-        this.ctx.clearRect(0, 0, 500, 500);
+        // linear interpolation using remaining accumulator value TODO
+        const interpolation = this.acc / this.dt;
+
         this.allBodies.forEach(e => {
             e.draw(this.ctx);
             if (this.debug) {
@@ -109,9 +171,18 @@ export class World {
         });
     }
 
+    /**
+     * Launch simulation event loop.
+     * @param {*} t 
+     */
     run(t) {
-        this.step(t);
+        if (t == undefined) {
+            t = performance.now();
+        }
+        t /= 1000; // scale time to seconds
+
         this.render(t);
+        this.update(t);
         requestAnimationFrame(t => this.run(t));
     }
 }
